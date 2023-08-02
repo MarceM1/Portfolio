29bacbddadaec4204eff87c39cb8530ddde2185e
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-08-02 11:21:21 -0300

    Cambios menores

--- scripts.js
+++ scripts.js
@@ -1,48 +1,48 @@
 window.sr = ScrollReveal();
     sr.reveal(`#nombre`, {
-         duration: 6000,
+         duration: 12000,
          origin: `right`,
         distance:  `-1000px`
 
     }); 
     sr.reveal(`#apellido`, {
-        duration: 8000,
+        duration: 16000,
         origin: `right`,
         distance:  `-1000px`
 
     }); 
     sr.reveal(`#puesto`, {
-        duration: 7000,
+        duration: 14000,
         origin: `right`,
         distance:  `-300px`
 
     });
     sr.reveal(`#contacto`, {
-        duration: 6000,
+        duration: 12000,
         origin: `top`,
         distance:  `-300px`
 
     });
     sr.reveal(`#icons   `, {
-        duration: 7000,
+        duration: 14000,
         origin: `top`,
         distance:  `-300px`
 
     });
     sr.reveal(`#texto1   `, {
-        duration: 6000,
+        duration: 12000,
         origin: `bottom`,
         distance:  `-200px`
 
     });
     sr.reveal(`#texto2   `, {
-        duration: 6000,
+        duration: 12000,
         origin: `left`,
         distance:  `-200px`
 
     });
     sr.reveal(`#texto3   `, {
-        duration: 6000,
+        duration: 12000,
         origin: `top`,
         distance:  `-200px`
 
