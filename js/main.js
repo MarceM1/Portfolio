f21370613804d47deb338209582da6258620eb63
Marcelo Melogno <marcelomelogno1986@gmail.com>
2024-03-12 01:44:11 -0300

    galeria 2 version pruebas

--- js/main.js
+++ js/main.js
@@ -111,48 +111,35 @@ particlesJS({
   retina_detect: true,
 });
 
-new Glide('.glide', {
-  type: 'carousel',
-  startAt: 0,
-  perView: 1,
-  gap: 30,
-  breackpoints: {
-      991: {
-          perViev:2
-      },
-      768: {
-          perViev:1
-      }
-  }
-}).mount();
 
 
-
-
-let hasClicked = false;
-
-function changeOpacity() {
-  const gridIcon = document.getElementById('grid-icon');
-  const carrousel = document.getElementById('carrousel');
-
-  if (!gridIcon || !carrousel) {
-    console.error("Elementos #grid-icon o #carrousel no encontrados.");
-    return; 
-  }
-
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
   
-  if (hasClicked) return;
 
-  hasClicked = true;
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
-  if(gridIcon.classList.contains('opacity-100')){
-    gridIcon.classList.toggle('opacity-0');
-    // gridIcon.classList.add('d-none');
-    carrousel.classList.toggle('opacity-100');
-  }
+});
 
-  gridIcon.style.display = gridIcon.classList.contains('opacity-0') ? 'none' : 'block';
-}
+
+
+
+
 
 
 
