599b156890b9d62d55f4e62e7c1ad9ea8d210ea6
Marcelo Melogno <marcelomelogno1986@gmail.com>
2024-03-20 22:44:40 -0300

    Skills

--- js/scripts.js
+++ js/scripts.js
@@ -2,36 +2,36 @@
 
 window.sr = ScrollReveal();
 sr.reveal('.show',{
-  duration: 7200,
+  duration: 6200,
   
 })
 sr.reveal(`.toleft`, {
-  duration:5400,
+  duration:4300,
   origin: `left`,
   distance: `-600px`,
 });
 sr.reveal(`.toleftslow`, {
-  duration: 6300,
+  duration: 5000,
   origin: `left`,
   distance: `-600px`,
 });
 sr.reveal(`.toleftextraslow`, {
-  duration: 7200,
+  duration: 5500,
   origin: `left`,
   distance: `-600px`,
 });
 sr.reveal(`.toright`, {
-  duration: 5400,
+  duration:4300,
   origin: `right`,
   distance: `-600px`,
 });
 sr.reveal(`.torightslow`, {
-  duration: 6300,
+  duration: 5000,
   origin: `right`,
   distance: `-600px`,
 });
 sr.reveal(`.torightextraslow`, {
-  duration: 7200,
+  duration: 5500,
   origin: `right`,
   distance: `-600px`,
 });
